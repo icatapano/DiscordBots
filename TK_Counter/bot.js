eb0613e276b8d205c7c47c285803af27c7aead08
Ian <nator419@yahoo.com>
2021-01-13 14:24:59 -0500

    Cleaned up code, ready for DB
    I cleaned up the code and commented it. Now it is time to work on getting it to store to a DB.

--- TK_Counter/bot.js
+++ TK_Counter/bot.js
@@ -1,67 +1,81 @@
-const Discord = require('discord.js')
-const client = new Discord.Client()
-const config = require('./config.json')
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const auth = require('./auth.json');
+// const Keyv = require('keyv');
+// const keyv = new keyv(auth.MONGO_CONNECT)
 
-var teamkillers = [] // Empty array to be populated of the teamkillers.
-var killCount = [] // Corrisponding kill count for perso who killed.
+var teamkillers = []; // Empty array to be populated with the teamkillers, this will be removed once DB is added.
+var killCount = []; // Corrisponding kill count for person commited the kill, also will be removed with the addition of the DB.
 
 client.on('message', receivedMessage => {
-  if (receivedMessage.author == client.user) { // Prevent bot from responding to its own messages
-      return
+  if (receivedMessage.author == client.user) { // Prevent bot from responding to itself and causing a infinite loop.
+      return;
   }
   
-  if (receivedMessage.content.startsWith("!")) {
-      processCommand(receivedMessage)
+  if (receivedMessage.content.startsWith("!")) { // Check to see if we have a bot command being sent.
+      processInput(receivedMessage);
   }
 })
 
-function processCommand(receivedMessage) {
-  let fullCommand = receivedMessage.content.substr(1) // Remove the leading exclamation mark
-  let splitCommand = fullCommand.split(" ") // Split the message up in to pieces for each space
-  let primaryCommand = splitCommand[0] // The first word directly after the exclamation is the command
-  let argument = receivedMessage.mentions.users.first() //splitCommand[1] // This should be the @username
+function processInput(receivedMessage) {
+  let splitInput = receivedMessage.content.split(" "); // Split the menu option from the tagged user if there is one.
+  let menuChoice = splitInput[0]; // Variable for the exclamation command word.
 
-  console.log("Command received: " + primaryCommand)
-  console.log("Argument: " + argument) // There may not be any arguments
+  console.log("Command received: " + menuChoice); // Used for debugging but sending this information to the console window.
 
-  if (primaryCommand == "help") {
-      helpCommand(receivedMessage)
-  } else if (primaryCommand == "teamkill") {
-      teamkillCounter(argument, receivedMessage)
-  } else if (primaryCommand == "total") {
-      displayCount(client) 
-  }else {
-      receivedMessage.channel.send("I don't understand the command. Try `!help`, `!teamkill`, or '!total'")
+  // Switch statement used to call the correct function for the correct command.
+  switch (menuChoice) {
+    case '!help':
+      helpCommand(receivedMessage);
+      break;
+    case '!teamkill':
+      teamkillCounter(receivedMessage);
+      break;
+    case '!total':
+      displayCount(receivedMessage);
+      break;
+    default:
+      receivedMessage.channel.send("I don't understand the command. Try `!help`, `!teamkill`, or `!total`");
   }
 }
 
+// List off the various commands and how to use them for the user.
 function helpCommand(receivedMessage) {
-  receivedMessage.reply('To add 1 kill to a user, issue the !teamkill command, followed by the @ tagged user but seperated with a space.')
-  receivedMessage.reply('Or use !total to display the current list of players and their total teamkills.')
+  receivedMessage.reply('To add 1 kill to a user, issue the !teamkill command, followed by the @ tagged user but seperated with a space.');
+  receivedMessage.reply('Or use !total to display the current list of players and their total teamkills.');
 }
 
-function teamkillCounter(argument, receivedMessage) {
+// Add the user ID if it doesn't exist and increament the counter for that ID by 1.
+function teamkillCounter(receivedMessage) {
+  let argument = receivedMessage.mentions.users.first(); // Retrieves the user's ID and assigns to the variable
+
+  console.log("Argument: " + argument); // Used for debugging but sending this information to the console window.
+
   if (!argument) {
-    return receivedMessage.reply('you need to follow the !teamkill with the tagged user!')
+    return receivedMessage.reply('you need to follow the !teamkill with the tagged user!'); // Command was issued without a tagged user, reminder of how it should be used.
   }
-  var i = 0
-  var found = false
+  let i = 0; // Counter for array elements.
+  let found = false; // Flag to identify if ID exists or not.
+
+  // Step through array checking if ID exist and adding a kill to the count.
   teamkillers.forEach((value) => {
       if (value == argument){
-        killCount[i] = killCount[i] + 1
-        found = true
+        killCount[i] = killCount[i] + 1;
+        found = true;
       }else{
-        i = i + 1
+        i = i + 1; 
       }
   })
+  // If ID wasnt found, it is added here with a kill count of 1.
   if (found == false){
-    teamkillers[i] = argument
-    killCount[i] = 1
+    teamkillers[i] = argument; 
+    killCount[i] = 1;
   }
-  displayCount(receivedMessage)
+  displayCount(receivedMessage);
 }
 
-function displayCount (receivedMessage) {
+// Displays all users and the total of friendly kills they have.
+function displayCount(receivedMessage) {
   i = 0
   teamkillers.forEach((value) => {
     receivedMessage.channel.send(value.username + " has a total of " + killCount[i] + " teamkills.")
@@ -69,4 +83,4 @@ function displayCount (receivedMessage) {
   })
 }
 
-client.login(config.DISCORD_TOKEN)
+client.login(auth.DISCORD_TOKEN) // Logs bot in with it's token.
