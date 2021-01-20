69c43d450999946c251e243d020eae464b893fea
Ian <nator419@yahoo.com>
2021-01-20 08:40:41 -0500

    Final product
    This is working as I wanted it to. I will be adding new features as the come to mind, but this is what was intended originally.

--- TK_Counter/bot.js
+++ TK_Counter/bot.js
@@ -4,8 +4,13 @@ const auth = require('./auth.json');
 const PlayerModel = require('./Player');
 const mongoose = require('mongoose');
 
-//client.on('ready', () => console.info(`logged in as ${client.user.tag}`));
+// Log bot login and set a status message.
+client.on("ready", () => {
+  client.user.setActivity("for !help", {type: "WATCHING"});
+  console.log(`Logged in as ${client.user.tag}!`);
+});
 
+// Check if a command was sent to the bot.
 client.on('message', async (receivedMessage) => {
   if (receivedMessage.author == client.user) { // Prevent bot from responding to itself and causing a infinite loop.
       return;
@@ -16,6 +21,7 @@ client.on('message', async (receivedMessage) => {
   }
 });
 
+// Connect to MongoDB
 (async () => {
   await mongoose.connect(auth.MONGO_CONNECT, {
     useNewUrlParser: true,
@@ -25,6 +31,8 @@ client.on('message', async (receivedMessage) => {
   });
 })()
 
+
+// Determine which command the user issued.
 function processInput(receivedMessage) {
   let splitInput = receivedMessage.content.split(" "); // Split the menu option from the tagged user if there is one.
   let menuChoice = splitInput[0]; // Variable for the exclamation command word.
@@ -49,39 +57,42 @@ function processInput(receivedMessage) {
       removeUser(receivedMessage);
       break;
     default:
-      receivedMessage.channel.send("I don't understand the command. Try `!help`, `!teamkill`, or `!total`");
+      receivedMessage.channel.send("I don't understand the command. Try `!help` for more information.");
   }
 }
 
 // List off the various commands and how to use them for the user.
 function helpCommand(receivedMessage) {
-  receivedMessage.reply('To add 1 kill to a user, issue the !teamkill command, followed by the @ tagged user but seperated with a space.');
-  receivedMessage.reply('Or use !total to display the current list of players and their total teamkills.');
+  receivedMessage.channel.send("<!teamkill @username> will add 1 teamkill to that user.");
+  receivedMessage.channel.send("<!removekill @username> will subtract 1 teamkill from that user. This is for when mistakes are made, don't make me block you!");
+  receivedMessage.channel.send("<!delete @username> will completely remove that user from the database. Again, don't make me!");
+  receivedMessage.channel.send("<!total> will display all users with teamkills and their counts.");
 }
 
 // Add the user ID if it doesn't exist and increament the counter for that ID by 1.
 async function teamkillCounter(receivedMessage) {
-  let argument = receivedMessage.mentions.users.first(); // Retrieves the user's ID and assigns to the variable
+  let argument = receivedMessage.mentions.users.first().username; // Retrieves the user's ID and assigns to the variable
 
   console.log("Argument: " + argument); // Used for debugging but sending this information to the console window.
 
   if (!argument) {
-    return receivedMessage.reply('You need to follow the !teamkill with the tagged user!'); // Command was issued without a tagged user, reminder of how it should be used.
+    return receivedMessage.reply('Error! No @username followed the command. Type <!help> for more informaiton.'); // Command was issued without a tagged user, reminder of how it should be used.
   } else {
-    await PlayerModel.findOneAndUpdate({_id: receivedMessage.mentions.users.first()}, { $inc: {count: 1} }, {upsert: true, new: true});
+    await PlayerModel.findOneAndUpdate({_id: receivedMessage.mentions.users.first().username}, { $inc: {count: 1} }, {upsert: true, new: true});
   }
+  displayCount(receivedMessage);
 }
 
 // Remove one from the teamkill counter on the specified user, intended for if a mistake was made.
 async function subtractTeamkill(receivedMessage) {
-  let argument = receivedMessage.mentions.users.first(); // Retrieves the user's ID and assigns to the variable
+  let argument = receivedMessage.mentions.users.first().username; // Retrieves the user's ID and assigns to the variable
 
   console.log("Argument: " + argument); // Used for debugging but sending this information to the console window.
 
   if (!argument) {
-    return receivedMessage.reply('You need to follow the !removekill with the tagged user!'); // Command was issued without a tagged user, reminder of how it should be used.
+    return receivedMessage.reply('Error! No @username followed the command. Type <!help> for more informaiton.'); // Command was issued without a tagged user, reminder of how it should be used.
   } else {
-    await PlayerModel.findOneAndUpdate({_id: receivedMessage.mentions.users.first()}, { $inc: {count: -1} }, {new: true});
+    await PlayerModel.findOneAndUpdate({_id: receivedMessage.mentions.users.first().username}, { $inc: {count: -1} }, {new: true});
   }
 
   displayCount(receivedMessage);
@@ -89,15 +100,15 @@ async function subtractTeamkill(receivedMessage) {
 
 // Remove user from database
 async function removeUser(receivedMessage) {
-  let argument = receivedMessage.mentions.users.first(); // Retrieves the user's ID and assigns to the variable
+  let argument = receivedMessage.mentions.users.first().username; // Retrieves the user's ID and assigns to the variable
 
   console.log("Argument: " + argument); // Used for debugging but sending this information to the console window.
 
   if (!argument) {
-    return receivedMessage.reply('You need to follow the !delete with the tagged user!'); // Command was issued without a tagged user, reminder of how it should be used.
+    return receivedMessage.reply('Error! No @username followed the command. Type <!help> for more informaiton.'); // Command was issued without a tagged user, reminder of how it should be used.
   } else {
     try {
-      await PlayerModel.deleteOne({_id: receivedMessage.mentions.users.first()});
+      await PlayerModel.deleteOne({_id: receivedMessage.mentions.users.first().username});
     } catch (error) {
       console.error(error);
     }
@@ -118,4 +129,4 @@ async function displayCount(receivedMessage) {
   }
 }
 
-client.login(auth.DISCORD_TOKEN) // Logs bot in with it's token.
+client.login(auth.DISCORD_TOKEN); // Logs bot in with it's token.
